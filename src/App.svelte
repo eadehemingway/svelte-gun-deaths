5176b556a40cd70b8897483c2644695f5cf4386a
Eade Hemingway <eadehemingway@Eades-MacBook-Pro.local>
2021-12-24 15:49:26 +0000

    Mostly working

--- src/App.svelte
+++ src/App.svelte
@@ -2,104 +2,174 @@
   import * as d3 from "d3";
   import { draw } from "svelte/transition";
   import * as easingFns from "svelte/easing";
-  import type { TemplateState } from "../template";
+  import type { Dataset, DataRow, TemplateState } from "../template";
   import { findYOnPathFromX } from "./utils";
 
+  export let state: TemplateState;
+  export let data: Dataset<DataRow>;
+
+  $: data_with_paths =
+    data.map((d) => ({
+      ...d,
+      path: undefined,
+      life_expectancy: Math.random() * 25 + 60,
+    })) || [];
+
+  const oranges = [
+    "rgb(136,51,6)",
+    "rgb(185,132,20)",
+    "rgb(219,115,4)",
+    "rgb(229,94,5)",
+    "rgb(153,103,27)",
+  ];
   const base_line_y = 250;
   const start_coords = [100, base_line_y];
+  const durations = [4000, 2000, 1500, 1000, 1000];
+  const delay = 400;
+
+  function getDuration(i) {
+    return i < durations.length ? durations[i] : 400;
+  }
+
+  function getDelay(i) {
+    if (i == 0) return 0;
+    const duration_so_far = durations
+      .slice(0, i)
+      .reduce((acc, cur) => acc + cur);
+    if (i < durations.length) {
+      return duration_so_far + delay;
+    }
+    return duration_so_far + i * delay;
+  }
 
   let dummy_data = [
-    // {
-    //   name: "jarule",
-    //   age_at_death: 30,
-    //   life_expectancy: 90,
-    //   visible: false,
-    //   path: undefined,
-    // },
-    { name: "ja", age_at_death: 90, life_expectancy: 100, visible: false, path: undefined },
+    {
+      id: "jarule",
+      age_at_death: 30,
+      life_expectancy: 90,
+      visible: false,
+      path: undefined,
+    },
+    { id: "jam", age_at_death: 40, life_expectancy: 100, path: undefined },
+    { id: "jaz", age_at_death: 30, life_expectancy: 98, path: undefined },
+    { id: "jazmin", age_at_death: 60, life_expectancy: 75, path: undefined },
+    { id: "james", age_at_death: 74, life_expectancy: 78, path: undefined },
   ];
-
-  export let state: TemplateState;
 
   const lineGenerator = d3.line().curve(d3.curveBasis);
   const xScale = d3.scaleLinear().domain([0, 100]).range([100, 600]);
-  const yScale = d3.scaleLinear().domain([0, 1]).range([200, 50]);
+  const yScale = d3.scaleLinear().domain([0, 1]).range([200, 100]);
 
-  function makeLivedLine(person) {
+  const randoms = {};
+
+  function makeLivedLine(person, i) {
+    const random = randoms[i] ? randoms[i] : Math.random();
+    randoms[i] = random;
+
     const end_x = xScale(person.life_expectancy);
-    // const mid_y = yScale(Math.random());
-    const mid_y = yScale(0.9);
-    const mid_x = xScale(person.life_expectancy / 2);
+    const mid_y = yScale(random);
+    const mid_one_x = xScale(person.life_expectancy / 4);
+    const mid_two_x = xScale((person.life_expectancy / 4) * 3);
 
-    return lineGenerator([start_coords, [mid_x, mid_y], [end_x, base_line_y]]);
+    return lineGenerator([
+      start_coords,
+      [mid_one_x, mid_y],
+      [mid_two_x, mid_y],
+      [end_x, base_line_y],
+    ]);
   }
 
-  const duration = 2000;
-  const delay = 400;
-  const animateIn = (node, { path, person, x, y, i }) => {
+  const animateDots = (node, { person, i }) => {
+    const path = node.parentNode.querySelector('path')
+    // const y = findYOnPathFromX(path, xScale(person.age_at_death));
+    const y = 20;
     const relative_y = base_line_y - y;
-    const relative_delay = i * delay;
+    const relative_delay = getDelay(i);
     const duration_until_point =
-      duration * (person.age_at_death / person.life_expectancy);
-
+      getDuration(i) * (person.age_at_death / person.life_expectancy);
 
     return {
       delay: relative_delay + duration_until_point,
-      duration: 500,
-      css: (t) => `transform: translateY(${t * relative_y - relative_y}px)`,
+      duration: 1000,
+      css: (t) => `
+        transform: translateY(${t * relative_y - relative_y}px);
+        opacity: ${t === 0 ? 0 : 1};
+      `,
+      easing: easingFns.quintIn,
     };
   };
+
+  function getWhiteLineDelay(person, i) {
+    const delay = getDelay(i);
+    if (i === 0) return delay + 500;
+    return delay;
+  }
 </script>
 
 <main class={state.theme === "light" ? "light" : "dark"}>
   <svg width="100%" height="100%">
-    <g>
-      {#each dummy_data as person, i (i)}
-        <clipPath id="orange-{person.name}">
-          <rect x={0} y=0 height={base_line_y} width={xScale(person.age_at_death)}>
-          </rect>
+    {#each data_with_paths as person, i (i)}
+      <g>
+        <clipPath id="orange-{person.id}">
+          <rect
+            x={0}
+            y="0"
+            height={base_line_y}
+            width={xScale(person.age_at_death)}
+          />
+        </clipPath>
+        <clipPath id="black-{person.id}">
+          <rect
+            x={xScale(person.age_at_death)}
+            y="0"
+            height={base_line_y}
+            width={800}
+          />
         </clipPath>
         <path
-          d={makeLivedLine(person)}
-          stroke="black"
-          stroke-width="1"
+          d={makeLivedLine(person, i)}
+          stroke="rgb(185,185,185)"
+          stroke-width="0.5"
           fill="none"
+          clip-path="url(#black-{person.id})"
           transition:draw={{
-            duration,
-            delay: i * delay,
+            duration: getDuration(i),
+            delay: getWhiteLineDelay(person, i),
             easing: easingFns.linear,
           }}
-          bind:this={person.path}
         />
         <path
-          d={makeLivedLine(person)}
-          stroke="orange"
+          d={makeLivedLine(person, i)}
+          stroke={oranges[(i + 1) % 5]}
           stroke-width="1"
-          clip-path="url(#orange-{person.name})"
+          clip-path="url(#orange-{person.id})"
           fill="none"
           transition:draw={{
-            duration,
-            delay: i * delay,
+            duration: getDuration(i),
+            delay: getDelay(i),
             easing: easingFns.linear,
           }}
-          bind:this={person.path}
-      />
+        />
         <circle
           r="2"
           cx={xScale(person.age_at_death)}
-          cy="250"
-          fill="orange"
-          in:animateIn={{
-            path: person.path,
+          cy={base_line_y}
+          fill={oranges[(i + 1) % 5]}
+          in:animateDots={{
             person,
-            x: xScale(person.age_at_death),
-            y: findYOnPathFromX(person.path, xScale(person.age_at_death)),
             i,
           }}
         />
-      {/each}
-    </g>
-    <line x1="100" y1={base_line_y} x2="600" y2={base_line_y} stroke="black" />
+      </g>
+    {/each}
+    <line
+      x1="100"
+      y1={base_line_y}
+      x2="600"
+      y2={base_line_y}
+      stroke="white"
+      stroke-width="0.5"
+    />
   </svg>
 </main>
 
@@ -111,7 +181,7 @@
     margin: 0;
   }
   main.light {
-    background-color: lightyellow;
+    background-color: rgb(20, 20, 20);
   }
   main.dark {
     background-color: darkblue;
--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,18 @@ import type { TemplateState } from '../template'
 export const state: TemplateState = {
   theme: 'light',
 }
-export const data = {};
+export const data = { data: []}
 
 let app;
 
-  export function update() {
-  app.$set({ state })
+export function update() {
+  app.$set({ state, data: data.data })
 }
 
 export function draw() {
   app = new App({
     target: document.body,
-    props: { state },
+    props: { state, data: data.data },
     intro: true,
   });
 }
